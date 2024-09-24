7ac044cd269f26264fbd8325cb1fa92b676abe9c
JBB <jbaptistebesancon@gmail.com>
2024-09-24 14:15:18 +0200

    Ajout component Header + Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,18 @@
 import '../styles/Header.css';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logoHeader.svg';
 
 function Header() {
   return (
     <header className="header">
-      <img src={logo} alt="logo de l'agence Kasa" />
+      <img src={logo} alt="logo de l'agence Kasa" className="header__logo" />
+      <nav className="header__navbar__mobile">
+        <span>ACCUEIL</span>
+        <span>A PROPOS</span>
+      </nav>
+      <nav className="header__navbar__desktop">
+        <span>Accueil</span>
+        <span>A Propos</span>
+      </nav>
     </header>
   );
 }
